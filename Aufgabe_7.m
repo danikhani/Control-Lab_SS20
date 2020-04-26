47d05b673333a7857a5445d8cc0648e9b095cc27
danikhani <30578398+danikhani@users.noreply.github.com>
2020-04-26 17:53:54 +0200

    Korrektur Aufgabe 7

--- Aufgabe_7.m
+++ Aufgabe_7.m
@@ -8,19 +8,16 @@ c_u = 885;
 
 
 s = tf('s');
-
-%Phasenreserve 30 mit P-Regler
 G_su = (m_u * s^2-c_u)/...
     ((d_s^2 - m_s ) * s^2 + (2*d_s*c_s - d_s )*s + (c_s^2 - c_s))
+
+%Phasenreserve 30 mit P-Regler
 G_r = 37.362
 G_o = G_su * G_r
 G_s = feedback(G_su, G_r)
 m = allmargin(G_o)
 
 figure(1)
-margin(G_su)
-grid
-figure(2)
 margin(G_o)
 grid
 
@@ -29,10 +26,8 @@ G_ri = 1/s
 G_oi = G_su * G_r
 n = allmargin(G_oi)
 
-figure(3)
+figure(2)
 margin(G_oi)
 grid
 
 %sisotool(G_su)
-
-%[Gm,Pm,Wcg,Wcp] = margin(G_s)
