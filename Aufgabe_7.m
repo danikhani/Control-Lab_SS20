6960786fdcefd2845f53b0bcb409fbd5236950db
danikhani <30578398+danikhani@users.noreply.github.com>
2020-04-27 10:29:18 +0200

    Aufgabe 7 repariert

--- Aufgabe_7.m
+++ Aufgabe_7.m
@@ -8,14 +8,14 @@ c_u = 885;
 
 
 s = tf('s');
-G_su_falsch = (m_u * s^2-c_u)/...
+G_su_old = (m_u * s^2-c_u)/...
     ((d_s^2 - m_s ) * s^2 + (2*d_s*c_s - d_s )*s + (c_s^2 - c_s))
 G_su = (m_u*s^2 + c_u)/...
-    ((d_s + c_s)^2 - (m_s*s^2 + d_s*s + c_s)*(m_u*s^2 + d_s*s + c_s + c_u))
+    ((d_s*s + c_s)^2 - (m_s*s^2 + d_s*s + c_s)*(m_u*s^2 + d_s*s + c_s + c_u))
 
 % skript seite 174
 %Phasenreserve -30 mit P-Regler
-G_r = 37.362
+G_r = 126.5
 G_o = G_su * G_r
 G_s = feedback(G_su, G_r)
 m = allmargin(G_o)
@@ -23,10 +23,10 @@ m = allmargin(G_o)
 figure(1)
 margin(G_o)
 grid
-sisotool(G_o)
+%sisotool(G_o)
 
 %Phasenreserve 30 mit I-Regler
-G_ri = 1/s
+G_ri = 1/s * 5.8846e+06
 G_oi = G_su * G_ri
 n = allmargin(G_oi)
 
@@ -34,4 +34,4 @@ figure(2)
 margin(G_oi)
 grid
 
-sisotool(G_oi)
+%sisotool(G_oi)
