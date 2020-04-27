5d9d209fcaca6197a3b6681c3c63552d8e1c2852
danikhani <30578398+danikhani@users.noreply.github.com>
2020-04-27 10:54:48 +0200

    Die Aufgabe 7 musste jetzt richtig sein

--- Aufgabe_7.m
+++ Aufgabe_7.m
@@ -15,7 +15,8 @@ G_su = (m_u*s^2 + c_u)/...
 
 % skript seite 174
 %Phasenreserve -30 mit P-Regler
-G_r = 126.5
+
+G_r = 104.2
 G_o = G_su * G_r
 G_s = feedback(G_su, G_r)
 m = allmargin(G_o)
@@ -26,7 +27,8 @@ grid
 %sisotool(G_o)
 
 %Phasenreserve 30 mit I-Regler
-G_ri = 1/s * 5.8846e+06
+%* 5.8846e+06
+G_ri = 1/s *800000
 G_oi = G_su * G_ri
 n = allmargin(G_oi)
 
