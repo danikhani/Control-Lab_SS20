9212675d9541503f9c5c1f8a6fe2b43c25ad6c01
danikhani <30578398+danikhani@users.noreply.github.com>
2020-04-30 10:39:10 +0200

    Aufgabe 11 und 9 wurden angepasst. vorhin war bei der Aufgabe 11 regelkreis falsch eingestellt

--- Aufgabe_11.m
+++ Aufgabe_11.m
@@ -18,6 +18,8 @@ c_u = 885;             % Federsteifigkeit c_u [N/m]
 % Dämpferkonstanten
 d_s = 0.142*10^3*0.1;  % Dämpferkonstante d_s [Ns/m]
 
+
+
 %% Aufgabe 11
 
 %--------------------------------------------
@@ -35,8 +37,16 @@ d_s = 0.142*10^3*0.1;  % Dämpferkonstante d_s [Ns/m]
 % a0_z = ;
 
 % Gsz = tf([b1_z,b0_z],[a4_z,a3_z,a2_z,a1_z,a0_z]);
-Gsz = c_u*(d_s*s+c_s)/((m_s*s^2+d_s*s+c_s)*(m_u*s^2+d_s*s+c_s+c_u)-(d_s*s+c_s)^2);
+s = tf('s');
+Gsz = c_u*(d_s*s+c_s)/((m_s*s^2+d_s*s+c_s)*(m_u*s^2+d_s*s+c_s+c_u)-(d_s*s+c_s)^2)
+
+Gsu = (m_u*s^2 + c_u)/...
+    ((d_s*s + c_s)^2 - (m_s*s^2 + d_s*s + c_s)*(m_u*s^2 + d_s*s + c_s + c_u))
 %--------------------------------------------
 % Reglerauslegung mit dem SISO-Tool
-sisotool(Gsz);
-G_PID = 2.5502*(1+0.15*s+ 0.077^2*s^2)/s
+%sisotool(Gsz);
+%sisotool(Gsu,1,1,Gsz);
+%sistool(g,c,h,f)
+G_PID = -13844*(1+0.12*s)*(1+0.12*s)/s
+
+
