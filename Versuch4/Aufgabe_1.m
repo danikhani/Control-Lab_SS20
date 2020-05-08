7e60b4495e4a1734ab9c2164c56094c5bf5cab67
DANIAL HEZARKHANI <danial.hezarkhani@rwth-aachen.de>
2020-05-08 15:35:25 +0200

    Aufgabe 4.8 ist fertig. TODO:Protocol muss geschrieben werden.

--- Versuch4/Aufgabe_1.m
+++ Versuch4/Aufgabe_1.m
@@ -6,5 +6,5 @@ V_punkt_3 = f(U_V101)*c*(V_2 - V_3)^0.5 - c*(V_3 - V_4)^0.5
 V_punkt_4 = c*(V_3 - V_4)^0.5 - f(U_V106)
 
 
-A = jacobian ([V_punkt_2,V_punkt_3,V_punkt_4],[V_2,V_3,V_4])
-B = jacobian ([V_punkt_2,V_punkt_3,V_punkt_4],[U_P101,U_V101,U_V106])
+A = jacobian([V_punkt_2,V_punkt_3,V_punkt_4],[V_2,V_3,V_4])
+B = jacobian([V_punkt_2,V_punkt_3,V_punkt_4],[U_P101,U_V101,U_V106])
--- Versuch4/Aufgabe_8_Vorlage.m
+++ Versuch4/Aufgabe_8_Vorlage.m
@@ -1,14 +1,34 @@
+
 %% Messdaten einlesen
 load('Aufgabe_8_Sprungantwort_Pumpe.mat');
-
+%y = cell2mat(Sprungantwort_Pumpe.OutputData);
+%u = cell2mat(Sprungantwort_Pumpe.InputData);
+%Ts = cell2mat(Sprungantwort_Pumpe.Ts);
 %% Eingangs- und Ausgangsverläufe plotten
 figure('name','Sprungantwort')
 plot(Sprungantwort_Pumpe)
 grid on
 
 %% Parameteridentifikation für PT1Tt-Verhalten mit der Funktion "tfest"
-%
-% Achtung: Die Ausgangsgröße wird in Liter (dm^3) angegeben
+np = 1;
+nz = 0;
+iodelay = NaN;
+sys = tfest(Sprungantwort_Pumpe,np,nz,iodelay)
+[K_Pumpe,T_Pumpe] = tfdata(sys);
+%Kpumpe = 0.1015
+%Tpumpe = 0.1991
+%Tt = 0.29
+
+% Zum überprüfen
+opt = stepDataOptions('InputOffset',50,'StepAmplitude',30);
+[y,t] = step(sys,opt);
+figure(2);
+plot(y);
+
+
+
+
+
 
 
 
