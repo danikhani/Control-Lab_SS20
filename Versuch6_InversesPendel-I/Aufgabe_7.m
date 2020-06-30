2143e47f9230a622d17d42b32c63991d4634f4b6
DANIAL HEZARKHANI <danial.hezarkhani@rwth-aachen.de>
2020-06-30 11:50:10 +0200

    versuch 6 completed

---
+++ Versuch6_InversesPendel-I/Aufgabe_7.m
@@ -0,0 +1,3 @@
+clear all;
+x = Simulink.BlockDiagram.getInitialState('Aufgabe_7_simulink')
+[A,B,C,D]=linmod('Aufgabe_7_simulink')
---
+++ Versuch6_InversesPendel-I/Aufgabe_8.m
@@ -0,0 +1,22 @@
+clear all
+%% Zustandsraum aus der Aufgabe 2
+A = [ 0,            1,                               0,           0;
+            0,          -50,                               0,           0;
+            0,            0,                               0,           1;
+            0, 2954700/5813, 7017652722578037/70368744177664, -22000/5813];
+B = [ 0 ; 50 ; 0 ; -2954700/5813];
+C= [ 1 , 0 , 0 , 0 ; 
+            0 , 0 , 1 , 0 ];
+D = [ 0 ; 0 ];
+        
+sys = ss(A,B,C,D)
+%% Stablität
+pzmap(sys)
+
+%% Steuerbarkeit
+rank(ctrb(sys))
+% voller Rang mit n =4
+
+%% Beobachtbarkeit
+rank(obsv(sys))
+% voller Rang mit n =4
